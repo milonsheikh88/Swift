6a927fd48a04654ac5f5fb9000f7204d66b21253
milon mac <milonsheikh88@gamil.com>
2021-07-06 06:36:01 +0600

    upload AccessControl

---
+++ 26-AccessControl.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+/*Swift Access Control: In swift, Access control is used to define a restriction on particular part of code to access any method, properties, types and functions based on programmer choice. The access control feature in swift enable us to hide the implementation details of our code and show only essentials features.
+ 
+ 1. Open or Public: access will enable entities to be used in any source file from the defined module and also in a source file from another module that import the defining module.
+ 2. Internal: access will enable entities to be used within any source file from their defining module, but not in any source file outside of that module. We can typically use internal when we define an app or framework internal structure.
+ 3. File-private: access will restrict the use of an entity to its own defining source file. We can use File-private access to hide the implementation details of a specific functionality when those details are used inside within an entire file.
+ 4. Private: access will enable entities to be used only within the enclosing declaration and we can use Private access to hide the implementation details of specific functionality when those details are used only within a single declaration.
+ */
